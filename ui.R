5067e080794594a3e3a3ca50fdacc3a8b163624c
Saw Simeon <doe_doe207@hotmail.com>
2016-08-26 17:02:35 +0700

    added dropdown button

--- ui.R
+++ ui.R
@@ -9,9 +9,13 @@ shinyUI(fluidPage(title="KNU/KNLA Peace Council Web Site", theme=shinytheme("cer
                              
                              tabPanel("History", titlePanel("History"), div(includeMarkdown("history.md"), align="justify")),
                              tabPanel("Department", titlePanel("Department"), includeMarkdown("department.md")),
-                             tabPanel("Activities", titlePanel("Activities"), div(includeMarkdown("activities.md"), align="justify")),
+                             #tabPanel("Activities", titlePanel("Activities"), div(includeMarkdown("activities.md"), align="justify")),
+                             navbarMenu("Agriculture", tabPanel(
+                               a("Rice Production", href="https://htmlpreview.github.io/?https://raw.githubusercontent.com/sawsimeon/pc-web/master/rice_farm.html", target = "_blank")),
+                                        tabPanel("Animal/Husbary", titlePanel("Animal")),
+                                        tabPanel("Chicken farm", titlePanel("Chicken"))),
                              tabPanel("Education", titlePanel("Education"), includeMarkdown("education.md")),
-                             tabPanel("Agriculture", titlePanel("Agriculture"), div(includeMarkdown("agriculture.md"), align="justify")),
+                             tabPanel("Activities", titlePanel("Activities"), div(includeMarkdown("activities.md"), align="justify")),
                              tabPanel("Meeting", titlePanel("Meeting"), includeMarkdown("meeting.md")),
                              tabPanel("Photos", titlePanel("Photos"), includeMarkdown("photo.md")),
                              tabPanel("Contact", titlePanel("Contact"), includeMarkdown("contact.md"))	
