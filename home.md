321a059f0198dddd4d16a1f497cfdf25fdebf4db
Saw Simeon <doe_doe207@hotmail.com>
2016-09-07 12:20:17 +0700

    update Tab Panel

--- home.md
+++ home.md
@@ -10,5 +10,8 @@
 <a href="http://www.youtube.com/watch?feature=player_embedded&v=DByWTCIeNuw" target="_blank">
 <img src="http://img.youtube.com/vi/DByWTCIeNuw/0.jpg" 
 alt="IMAGE ALT TEXT HERE" width="100%" height="100%" border="10" /></a>
-*Click The Image to play the video in new browser*
 
+
+<img src="https://raw.githubusercontent.com/sawsimeon/pc-web/master/img/It_Gen_Ler_Mu2.jpg" style = "width: 100%; height: 100%"/>
+
+<img src="https://raw.githubusercontent.com/sawsimeon/pc-web/master/img/It_Gen_Ler_Mu3.jpg" style = "width: 100%; height: 100%"/>
--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ library(markdown)
 
 shinyUI(fluidPage(title="KNU/KNLA Peace Council Web Site", theme=shinytheme("cerulean"),
                   navbarPage(strong("KNU/KNLA Peace Council"),
-                             tabPanel("Home", titlePanel("KNU/KNLA Peace Council"), includeMarkdown("home.md")), #tabPanel Submit Job # uiOutput("video")
+                             tabPanel("Home", titlePanel("KNU/KNLA Peace Council"),  includeMarkdown("home.md"), uiOutput("video")), #tabPanel Submit Job # uiOutput("video")
                              
                              tabPanel("History", titlePanel("History"), includeMarkdown("history.md")),
                              tabPanel("Department", titlePanel("Department"), includeMarkdown("department.md")),
@@ -14,10 +14,14 @@ shinyUI(fluidPage(title="KNU/KNLA Peace Council Web Site", theme=shinytheme("cer
                                a("Rice Production", href="https://htmlpreview.github.io/?https://raw.githubusercontent.com/sawsimeon/pc-web/master/rice_farm.html", target = "_blank")),
                                         tabPanel(a("Animal/Husbary", href = "https://htmlpreview.github.io/?https://raw.githubusercontent.com/sawsimeon/pc-web/master/animal.html", target = "_blank")),
                                         tabPanel(a("Chicken farm", href = "https://htmlpreview.github.io/?https://raw.githubusercontent.com/sawsimeon/pc-web/master/chicken.html", target = "_blank"))),
+                             tabPanel("Construction", titlePanel("Construction"), includeMarkdown("construction.md")),
                              tabPanel("Education", titlePanel("Education"), includeMarkdown("education.md")),
                              tabPanel("Activities", titlePanel("Activities"), includeMarkdown("activities.md")),
                              tabPanel("Meeting", titlePanel("Meeting"), includeMarkdown("meeting.md")),
+                             tabPanel("Karen Marthyr's Day", titlePanel("Karen Martyr's Day"), includeMarkdown("marthyr.md")),
+                             tabPanel("Karen New Year", titlePanel("Karen New Year"), includeMarkdown("new_year.md")),
                              tabPanel("Photos", titlePanel("Photos"), includeMarkdown("photo.md")),
+                             tabPanel("Videos", titlePanel("Videos"), includeMarkdown("video.md")),
                              tabPanel("Contact", titlePanel("Contact"), includeMarkdown("contact.md"))	
                              
                   ) #navbarPage
